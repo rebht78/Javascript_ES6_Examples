80c4fcb32a68ad20db563d5c68640d43c4a1ebdb
Sohel Shaikh <rebht78@gmail.com>
2018-10-16 22:30:55 +0530

    added arrow function examples

---
+++ src/arrowExample.js
@@ -0,0 +1,9 @@
+const square = number => {
+  return number * number;
+};
+
+let characters = ["Sherlock", "Jim", "John", "Mary"];
+
+console.log(characters.filter(item => item.length >= 5));
+
+console.log(square(20));
